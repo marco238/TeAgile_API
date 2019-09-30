faad28b2b595b60b205a4d9673e9f72a087f4e28
Marco <marco238cuba@gmail.com>
2019-09-30 16:21:35 +0200

    feat: create projects given an user Id

--- routes/projects.routes.js
+++ routes/projects.routes.js
@@ -7,18 +7,40 @@ const User = require('../models/user.model');
 
 /* GET projects listing by user's Id. */
 router.get('/:id', function(req, res, next) {
+  console.log(req.params);
   const id = req.params.id;
-  User.findById(id, function(err, user) {
-    res.send(user.projects);
-  });
+  User.findById(id)
+    .then(user => {
+      res.send(user.projects);
+    })
+    .catch(err => {
+      res.send(err.message, 204, err.errors);
+    });
 });
 
 /* POST create a project. */
 router.post('/', function(req, res, next) {
   const project = new Project(req.body);
-  project.save()
-    .then(() => {
-      res.status(201).json(project);
+  User.findById(req.body.user)
+    .then(user => {
+      if(user) {
+        user.projects.push(project._id);
+        User.updateOne({_id: req.body.user}, {projects: user.projects})
+          .then(() => {
+            project.save()
+              .then(() => {
+                res.status(201).json(project);
+              })
+              .catch(error => {
+                console.log(error);
+              });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      } else {
+        res.send('User not found in DB', 204);
+      }
     })
     .catch(error => {
       console.log(error);
